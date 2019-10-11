df4185b06c66765d1b79d6552434b2dbf4f60694
tongtong3 <tongtong3@yonyou.com>
2019-10-11 16:02:43 +0800

    React snippets

---
+++ src/componments/G.js
@@ -0,0 +1,17 @@
+/**
+ * @description: 根据React snippets plugins可以使用rcc快捷语句，快速创建一个简易组件。
+ */
+class componentName extends Component {
+    constructor(props, ...rest) {
+        super(props, ...rest);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+componentName.propTypes = {};
